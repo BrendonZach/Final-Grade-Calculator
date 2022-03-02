533dee528320185876dd29d54260dcfe307bfde5
Brendon Zach <brendon_zach@outlook.com>
2022-03-02 14:12:04 -0700

    added variables and outlets

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -9,14 +9,38 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    
 
-  
+    @IBOutlet weak var currentGrade: UITextField!
+    
+    @IBOutlet weak var desiredGrade: UITextField!
+    
+    @IBOutlet weak var finalGradeWeight: UITextField!
+    
+    @IBAction func Calculate(_ sender: Any)
+    {
+        var myGrade = currentGrade.text
+        var expectedGrade = desiredGrade.text
+        var gradeWeight = finalGradeWeight.text
+        var currentGradeint = Int(myGrade)
+        var desiredGradeint = Int(expectedGrade)
+        var finalGradeWeightin = Int(gradeWeight)
+        
+    }
+    
+    @IBOutlet weak var finalAnswer: UILabel!
+   
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
+    
+    
+    
+    
 }
 
